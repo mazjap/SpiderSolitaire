9d8575fbae3f107b7f94bd9813120da2ffc0652a
Jordan C <jordan.c4922@gmail.com>
2024-09-06 21:03:14 -0600

    Remove comment headers and format indents

--- SpiderSolitaire/Item.swift
+++ SpiderSolitaire/Item.swift
@@ -1,18 +1,11 @@
-//
-//  Item.swift
-//  SpiderSolitaire
-//
-//  Created by Jordan Christensen on 9/6/24.
-//
-
 import Foundation
 import SwiftData
 
 @Model
 final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
+  var timestamp: Date
+  
+  init(timestamp: Date) {
+    self.timestamp = timestamp
+  }
 }
--- SpiderSolitaireTests/SpiderSolitaireTests.swift
+++ SpiderSolitaireTests/SpiderSolitaireTests.swift
@@ -1,16 +1,7 @@
-//
-//  SpiderSolitaireTests.swift
-//  SpiderSolitaireTests
-//
-//  Created by Jordan Christensen on 9/6/24.
-//
-
 import Testing
 
 struct SpiderSolitaireTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
 }
