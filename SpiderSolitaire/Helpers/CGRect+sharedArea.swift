4b584c93208450477912e9022f76ee7f86a24dda
Jordan C <jordan.c4922@gmail.com>
2024-09-12 14:16:34 -0600

    Add shared area function

---
+++ SpiderSolitaire/Helpers/CGRect+sharedArea.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+extension CGRect {
+  func sharedArea(with rect: CGRect) -> Double {
+    let area = width * height
+    let otherArea = rect.width * rect.height
+    
+    let sharedWidth = min(maxX, rect.maxX) - max(minX, rect.minX)
+    let sharedHeight = min(maxY, rect.maxY) - max(minY, rect.minY)
+    let sharedArea = sharedWidth * sharedHeight
+    
+    let totalArea = area + otherArea - sharedArea
+    
+    return sharedArea / totalArea
+  }
+}
